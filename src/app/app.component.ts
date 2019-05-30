6b14436bf5a126d0b99abc54c1b91d361d0a5e9b
shahi-frontend <rupali.ceinc@gmail.com>
2019-05-30 12:05:18 +0530

    binding property and event binding and splitting 2 way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  title: string = 'Angular Component Interaction';
+  imgUrl: string = 'https://www.gstatic.com/webp/gallery/5.jpg';
+  count: number = 0;
+  name: string;
+
+  counterInc(){
+    this.count += 1;
+  }
+
+  nameChangeDec(nameE){
+    this.name = nameE;
+    if(nameE === 'Harvey') {
+      alert("Welcome back Harvey!");
+    }
+  }
 }
